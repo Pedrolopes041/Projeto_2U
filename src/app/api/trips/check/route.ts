aa024b953d10601c9475240f1b025d4fedcdd316
Pedro Sobrenome <pedroaugustolopesgomes@example.com>
2024-01-12 11:52:52 -0300

    fix: update

--- src/app/api/trips/check/route.ts
+++ src/app/api/trips/check/route.ts
@@ -1,8 +1,9 @@
 import { prisma } from "@/lib/prisma";
-import { differenceInDays, isBefore } from "date-fns";
 import { NextResponse } from "next/server";
+import { differenceInDays } from "date-fns";
 
 export async function POST(request: Request) {
+
   const req = await request.json();
 
   const trip = await prisma.trip.findUnique({
@@ -10,53 +11,15 @@ export async function POST(request: Request) {
       id: req.tripId,
     },
   });
-
-  if (!trip) {
-    return new NextResponse(
-      JSON.stringify({
-        error: {
-          code: "TRIP_NOT_FOUND",
-        },
-      })
-    );
-  }
-
-  if (isBefore(new Date(req.startDate), new Date(trip.startDate))) {
-    return new NextResponse(
-      JSON.stringify({
-        error: {
-          code: "INVALID_START_DATE",
-        },
-      }),
-      {
-        status: 400,
-      }
-    );
-  }
-
-  // Data de fim recebida precisa ser menor ou igual a data de fim da viagem
-  if (isBefore(new Date(trip.endDate), new Date(req.endDate))) {
-    return new NextResponse(
-      JSON.stringify({
-        error: {
-          code: "INVALID_END_DATE",
-        },
-      }),
-      {
-        status: 400,
-      }
-    );
-  }
-
   const reservations = await prisma.tripReservation.findMany({
     where: {
       tripId: req.tripId,
       // VERIFICA SE EXISTE RESERVA ENTRE AS DATAS
       startDate: {
-        lte: new Date(req.endDate),
+        lte: new Date(req.startDate),
       },
       endDate: {
-        gte: new Date(req.startDate),
+        gte: new Date(req.endDate),
       },
     },
   });
@@ -73,7 +36,7 @@ export async function POST(request: Request) {
     JSON.stringify({
       success: true,
       trip,
-      totalPrice: differenceInDays(new Date(req.endDate), new Date(req.startDate)) * Number(trip.pricePerDay),
+      totalPrice: differenceInDays(new Date(req.endDate), new Date(req.startDate)) * Number(trip?.pricePerDay),
     })
   );
 }
