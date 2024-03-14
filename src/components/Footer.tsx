45a79d932fd29a9f5b01c47e388070216e4c3951
Pedro Sobrenome <pedroaugustolopesgomes@example.com>
2024-03-14 11:25:34 -0300

    feat: update footer and loader

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,16 @@
-import Image from "next/image"
+import Image from "next/image";
 
 const Footer = () => {
+  return (
+    <div className="flex flex-col justify-center items-center p-5 bg-">
+      <h1 className="font-semibold text-2xl text-grayPrimary text-center">
+        GO<span className="text-primary">TRIPS!</span>
+      </h1>
+      <p className="text-sm font-semibold text-primaryDarker mt-1">
+        Todos os direitos reservados
+      </p>
+    </div>
+  );
+};
 
-    return(
-        <div className="flex flex-col justify-center items-center p-5 bg-">
-            <Image src="/logo.png" alt="logo" width={133} height={23}/>
-            <p className="text-sm font-semibold text-primaryDarker mt-1">Todos os direitos reservados</p>
-        </div>
-    )
-
-}
-
-export default Footer
+export default Footer;
